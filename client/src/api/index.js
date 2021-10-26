86815385ff898dfc4dc0e56218470064398dce0f
Loki1204 <loki1204@users.noreply.github.com>
2021-10-26 06:34:27 +0530

    testing for cors error

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://tic-tac-toe-game-socket-io.herokuapp.com/");
+const socket = io("https://tic-tac-toe-game-socket-io.herokuapp.com");
+// const socket = io("http://localhost:5000");
 
 export default socket;
--- server/server.js
+++ server/server.js
@@ -9,34 +9,36 @@ import socketEvents from "./controllers/socketEvents.js";
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+
 const httpServer = createServer(app);
 
 export const io = new Server(httpServer, {
   cors: {
-    origin: "https://tic-tac-toe-game-socket-io.netlify.app/",
+    origin: "https://tic-tac-toe-game-socket-io.netlify.app",
+    // origin: "http://localhost:3000",
     methods: ["GET", "POST", "PUT"],
   },
 });
 
 socketEvents();
 
-app.use(express.json());
-app.use(cors());
-
 app.get("/", (req, res) => {
   res.send("Welcome to Tic-Tac-Toe game API");
 });
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-const server = httpServer.listen(
-  PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
-  )
-);
+// const server = httpServer.listen(
+//   PORT,
+//   console.log(
+//     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+//   )
+// );
 
-process.on("unhandledRejection", (err, promise) => {
-  console.log(`Logged Error: ${err.message}`);
-  server.close(() => process.exit(1));
-});
+// process.on("unhandledRejection", (err, promise) => {
+//   console.log(`Logged Error: ${err.message}`);
+//   server.close(() => process.exit(1));
+// });
